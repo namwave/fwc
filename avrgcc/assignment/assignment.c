1065c9d605e5f5af874d46f1bdd4012db83d11ed
Namrath <namrathpneni@gmail.com>
2022-08-29 15:30:42 +0530

    Added avrgcc assignment

--- avrgcc/assignment/assignment.c
+++ avrgcc/assignment/assignment.c
@@ -1,4 +1,27 @@
-//Prints the number 0 or 8 using 
-//the if-else condition
 #include <avr/io.h>
-#include <util/delay.h>
+//#include <util/delay.h>
+
+int main (void)
+{
+	int X, Y;
+	int A=0, B=0, C=0;
+	
+	DDRB 	|= ((1 << DDB0));
+	
+	while(1) {
+		
+		//OR Condition 
+		X = (A||B)||C; // (A+B)+C
+		Y = A||(B||C);// A+(B+C)
+		
+		//AND Condition
+		// X = (A&&B)&&C; // (A.B).C
+		// Y = A&&(B&&C); // A.(B.C)
+		
+		if(X==Y)
+			PORTB = ((1 <<  PB0));
+		else if(X!=Y)
+			PORTB = ((0 <<  PB0));
+		}
+	return 0;
+}
