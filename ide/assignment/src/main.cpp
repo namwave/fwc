97cddbd67811b085ce6bdb95ec7d13977d2024e0
Namrath <namrathpneni@gmail.com>
2022-09-16 15:16:05 +0530

    adding few changes

--- ide/assignment/src/main.cpp
+++ ide/assignment/src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-int A=1,B=1,C=1;
+int A=0,B=0,C=0;
 int X,Y;
 
 void blink(int K,int L)
@@ -25,9 +25,9 @@ void setup()
 void loop()
 {
 	X = (A||B)||C;
-	Y = A||(B||C);
-	blink(X,Y);
-	delay(5000);
+	digitalWrite(8, X);
+//	Y = A||(B||C);
+//	blink(X,Y);
 }
 //&& is the AND operation
 // || is the OR operation
